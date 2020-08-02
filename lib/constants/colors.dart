1b6c866611555167a92ca5457b92c1e76eb2cd58
Nazeer Allahham <nazeer12allaham@gmail.com>
2020-08-02 21:48:40 +0300

    enhance user interfaces

---
+++ lib/constants/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color molarLightBlueColor = Color(0xffc3cde6);
+  static const Color molarRedColor = Color(0xffff0101);
+  static const Color molarRed1Color = Color(0xff9146e7);
+  static const Color molarRed2Color = Color(0xff785aec);
+  static const Color molarRed3Color = Color(0xffa832e0);
+  static const Color molarBlueColor = Color(0xff01008c);
+
+  static const Color primaryColor = Color(0xffffffff);
+  static const Color primaryColorDark = Color(0xff414141);
+  static const Color accentColor = Colors.lightGreen;
+
+  static const Color secondaryColor = Color(0xfff9f9f9);
+  static const Color primaryTextColor = Color(0xff414141);
+}
---
+++ lib/constants/strings.dart
@@ -0,0 +1,5 @@
+class Strings {
+  static String applicationTitle = "Molar Dent";
+  static String downloadUrl =
+      "https://molardentalmaterials.com/get?app=apk&version=latest";
+}
